373728e6fbf66343b64a61b2d255af91d53f75ef
jarrodskahill <jarrodskahill@gmail.com>
2021-07-29 21:23:52 -0500

    created a file and functional component for food list

--- src/components/tracker/tracker.js
+++ src/components/tracker/tracker.js
@@ -1,9 +1,13 @@
 import React from "react";
+import FoodList from "./foodList";
 
 const Tracker = () => {
   return (
     <div className="each">
-      <h2>Tracker</h2>
+      <h2>Food Tracker</h2>
+      <div className="todo">
+        <FoodList />
+      </div>
     </div>
   );
 };
